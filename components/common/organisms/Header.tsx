7ef3c9929a2e11570227ad1381a46134495427da
jongse7 <mmssjj72@naver.com>
2025-08-06 19:33:47 +0900

    feat: header에 className 추가

--- components/common/organisms/Header.tsx
+++ components/common/organisms/Header.tsx
@@ -2,18 +2,26 @@ import Button from "@/components/common/Button";
 import BetalabLogo from "@/components/common/svg/BetalabLogo";
 import HeaderIcons from "../molecules/HeaderIcons";
 import Searchbar from "@/components/common/molecules/Searchbar";
+import { cn } from "@/lib/utils";
 
 interface HeaderProps {
   isLogin?: boolean;
   isSearchbar?: boolean;
+  className?: string;
 }
 
 export default function Header({
   isLogin = false,
   isSearchbar = false,
+  className,
 }: HeaderProps) {
   return (
-    <header className="w-full flex flex-row items-center px-16 py-4 justify-between">
+    <header
+      className={cn(
+        "w-full flex flex-row items-center px-16 py-4 justify-between",
+        className
+      )}
+    >
       <div className="flex h-11 flex-row items-center gap-4">
         <BetalabLogo className="w-25 h-6" />
         {isSearchbar && <Searchbar />}
@@ -32,9 +40,14 @@ export default function Header({
   );
 }
 
-export function HeaderAdmin() {
+export function HeaderAdmin({ className }: { className?: string }) {
   return (
-    <header className="w-full flex flex-row items-center px-16 py-4 justify-between">
+    <header
+      className={cn(
+        "w-full flex flex-row items-center px-16 py-4 justify-between",
+        className
+      )}
+    >
       <div className="flex h-11 flex-row items-center gap-3">
         <BetalabLogo className="w-25 h-6" />
         <h3 className="text-body-01 font-semibold text-Dark-Gray">관리자용</h3>
