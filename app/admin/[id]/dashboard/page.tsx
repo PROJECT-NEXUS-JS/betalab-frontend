ded5a67498ae048c19ea94776109050051dc845a
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-08-25 01:29:29 +0900

    feat: 대시보드에 테스트 제목 불러오기

--- app/admin/[id]/dashboard/page.tsx
+++ app/admin/[id]/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { queryKeys } from '@/constants/query-keys';
 import Toggle from '@/components/common/atoms/Toggle';
 import StatsCardClientWrapper from './StatsCardClientWrapper';
 import BarChartClientWrapper from './BarChartClientWrapper';
+import TestTitleClient from './TestTitleClient';
 
 import Logger from '@/lib/logger';
 import { StatsResponseSchema } from '@/hooks/dashboard/quries/useStatsQuery';
@@ -38,9 +39,7 @@ export default async function AdminDashboardPage({ params }: { params: Promise<{
   return (
     <div className="flex flex-col w-full max-w-[854px] mb-40">
       <section className="flex justify-between items-center w-full">
-        <h2 className="text-2xl font-bold text-Black">
-          테스트의 제목을 적어주세요 최대 두줄까지 보여집니다(추후 수정 예정)
-        </h2>
+        <TestTitleClient id={id} />
         <div className="flex gap-2 items-center">
           <p className="text-base font-bold text-Dark-Gray">모집중</p>
           <Toggle checked={true} />
