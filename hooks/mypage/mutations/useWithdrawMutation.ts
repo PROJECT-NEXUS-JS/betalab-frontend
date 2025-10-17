ef619bf56264378255c3377862e766caf00e60e6
Tnalxmsk <alstnwkd990@naver.com>
2025-10-18 01:01:31 +0900

    feat: 회원 탈퇴 기능 구현
    - useWithdrawMutation onSuccess 로직 추가 구현
    - BetalabModal 구조 개선

--- hooks/mypage/mutations/useWithdrawMutation.ts
+++ hooks/mypage/mutations/useWithdrawMutation.ts
@@ -1,6 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 import { instance } from '@/apis/instance';
 import { WithdrawRequestType, WithdrawResponseType } from '../dto/withdraw';
+import { useRouter } from 'next/navigation';
+import { Dispatch, SetStateAction } from 'react';
 
 const withdraw = async (data: WithdrawRequestType): Promise<WithdrawResponseType> => {
   const params = new URLSearchParams();
@@ -14,8 +16,15 @@ const withdraw = async (data: WithdrawRequestType): Promise<WithdrawResponseType
   return response.data;
 };
 
-export const useWithdrawMutation = () => {
+export const useWithdrawMutation = (closeModal:  Dispatch<SetStateAction<boolean>>) => {
+  const router = useRouter();
   return useMutation({
     mutationFn: withdraw,
+    onSuccess: () => {
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+      closeModal(false);
+      router.replace('/login');
+    },
   });
 };
